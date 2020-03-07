52f406a17fe80b9527ba06e48eaaa00710eed8cc
Crystal Shir Navarro <crystal.shir@gmail.com>
2020-03-06 23:54:46 -0800

    created search results with react hooks and petfinder api

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,17 +1,33 @@
-import React, { useState } from 'react';
-import { unstable_renderSubtreeIntoContainer } from 'react-dom/cjs/react-dom.development';
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown"
 
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
+    const [breeds, setBreeds] = useState([]);
+    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+    const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+    useEffect(() => {
+        setBreeds([]);
+        setBreed("");
+
+        pet.breeds(animal).then(({ breeds }) => {
+            const breedStrings = breeds.map(({ name }) => name);
+            setBreeds(breedStrings);
+        }, console.error);
+    }, [animal, setBreed, setBreeds]);
+
 
     return (
         <div className="search-params">
-            <h1>{location}</h1>
             <form>
                 <label htmlFor="location">
-                    location
+                    Location
                     <input id="location" value={location} placecholder={Location} onChange={e => setLocation(e.target.value)} />
                 </label>
+                <AnimalDropdown />
+                <BreedDropdown />
                 <button>Submit</button>
             </form>
         </div>
